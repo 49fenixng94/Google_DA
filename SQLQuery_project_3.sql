e4f627d110e0cf624f4ee678eb8160d7040171a8
49fenixng94 <85995794+49fenixng94@users.noreply.github.com>
2021-07-05 15:20:34 +0800

    Delete SQLQuery_project_3.sql

--- SQLQuery_project_3.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-db.member_casual AS user_type,
-year(db.started_at)as yr,
-month(db.started_at) as mth,
-count(ride_id) as num_users
-
-FROM bike_share.dbo.[12mth_combined] AS db
-group by db.member_casual, year(db.started_at), month(db.started_at)
-order by db.member_casual, year(db.started_at), month(db.started_at)
